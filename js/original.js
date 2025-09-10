8844a520704274b0a0afc62bc0083aa2d2666297
ciberjaam <124893809+ciberjaam@users.noreply.github.com>
2025-09-09 21:07:10 -0400

    gemini analysis final

--- js/original.js
+++ js/original.js
@@ -1347,7 +1347,7 @@
 
             if ('serviceWorker' in navigator) {
                 window.addEventListener('load', () => {
-                    navigator.serviceWorker.register('./sw.js').then(reg => {
+                    location.hostname.match(/^(127\.0\.0\.1|localhost)$/) ? Promise.resolve() : navigator.serviceWorker.register('./sw.js').then(reg => {
                         console.log('Service worker registered!', reg);
                     }).catch(err => {
                         console.log('Service worker registration failed: ', err);
